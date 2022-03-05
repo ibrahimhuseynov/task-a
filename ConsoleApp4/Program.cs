4af3904799a8f49fdfe08f39c4e2640a7f231e51
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-05 11:51:00 +0400

    new project

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string soz = Console.ReadLine();
+            int say = 0;
+            for (int i = 0; i < soz.Length; i++)
+            {
+                if (soz[i] == 'a')
+                {
+                    say++;
+                    
+                }
+            }
+            Console.WriteLine(say);
+            Console.ReadLine();
+        }
+    }
+}
